82e7c54bbb81101f7201de65c3e7d8deaa860ec3
edge121212 <edge.mg11@nycu.edu.tw>
2026-05-27 19:42:46 +0800

    feat: Add argparse to download_msd_aws.py to allow specific task downloads

--- dataset/download_msd_aws.py
+++ dataset/download_msd_aws.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 import tarfile
+import argparse
 
 def download_and_extract(url, target_path):
     print(f"開始下載 {target_path} ... (檔案較大，請耐心等候)")
@@ -24,22 +25,34 @@ def download_and_extract(url, target_path):
         safe_extract(tar)
     print(f"{target_path} 解壓縮完畢！\n")
 
-tasks = [
-    "Task05_Prostate.tar",
-    "Task06_Lung.tar",
-    "Task07_Pancreas.tar"
-]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download MSD datasets")
+    parser.add_argument("--task", type=str, default="All", 
+                        choices=["Prostate", "Lung", "Pancreas", "All"],
+                        help="Choose which dataset to download: Prostate, Lung, Pancreas, or All")
+    args = parser.parse_args()
 
-base_url = "https://msd-for-monai.s3.amazonaws.com/"
+    all_tasks = {
+        "Prostate": "Task05_Prostate.tar",
+        "Lung": "Task06_Lung.tar",
+        "Pancreas": "Task07_Pancreas.tar"
+    }
 
-for task in tasks:
-    url = base_url + task
-    target_path = task
-    extracted_folder = task.replace('.tar', '')
-    
-    if not os.path.exists(extracted_folder):
-        download_and_extract(url, target_path)
+    if args.task == "All":
+        tasks_to_download = list(all_tasks.values())
     else:
-        print(f"{extracted_folder} 已經存在，跳過下載。")
+        tasks_to_download = [all_tasks[args.task]]
 
-print("所有指定資料集已準備就緒！")
+    base_url = "https://msd-for-monai.s3.amazonaws.com/"
+
+    for task in tasks_to_download:
+        url = base_url + task
+        target_path = task
+        extracted_folder = task.replace('.tar', '')
+        
+        if not os.path.exists(extracted_folder):
+            download_and_extract(url, target_path)
+        else:
+            print(f"{extracted_folder} 已經存在，跳過下載。")
+
+    print("所有指定資料集處理完畢！")
